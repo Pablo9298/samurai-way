3251ecd496ae585538db971f85b17ecefb24b6a4
Pablo9298 <wmpawka@gmail.com>
2023-11-09 21:01:43 +0200

    added styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import './App.css'
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Profile from "./components/Profile";
+import './App.css';
+import {Header} from './layout/header/Header';
+import {Navbar} from './layout/navbar/Navbar';
+import {Profile} from './layout/profile/Profile';
+import styled from "styled-components";
 
 const App = () => {
     return (
-        <div className="app-wrapper">
+        <AppStyled>
             <Header/>
             <Navbar/>
             <Profile/>
-        </div>
+        </AppStyled>
     );
 }
 
 export default App;
+
+const AppStyled = styled.div`
+  display: grid;
+  grid-template-areas:
+    "h h"
+    "n c";
+  grid-template-rows: 60px 1fr;
+  grid-template-columns: 2fr 10fr;
+  grid-gap: 10px;
+  height: 100vh;
+`
